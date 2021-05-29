d02eb5440fd6032b0de69f7984c515376cc8dfca
Avijit Bairagi <ovi821@gmail.com>
2021-05-29 13:17:03 +0600

    web socket impl

--- src/main/java/com/example/fileuploader/lintener/RabbitMqListener.java
+++ src/main/java/com/example/fileuploader/lintener/RabbitMqListener.java
@@ -2,12 +2,16 @@ package com.example.fileuploader.lintener;
 
 import com.example.fileuploader.common.Constants;
 import com.example.fileuploader.model.MqTask;
+import com.example.fileuploader.model.OutputMessage;
 import com.example.fileuploader.service.ImageProcessingService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Configuration
 public class RabbitMqListener {
@@ -15,9 +19,20 @@ public class RabbitMqListener {
     @Autowired
     private ImageProcessingService imageProcessingService;
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @RabbitListener(queues = Constants.IMAGE_PROCESSING_QUEUE)
     public void listen(MqTask task) throws IOException {
         System.out.println("Message read from myQueue : " + task);
         imageProcessingService.process(task);
+
+        publishToWebSocket();
+    }
+
+    private void publishToWebSocket() {
+
+        simpMessagingTemplate.convertAndSend("/topic/image-resize",
+                new OutputMessage("Image resized Successfully.", new SimpleDateFormat("HH:mm").format(new Date())));
     }
 }
---
+++ src/main/java/com/example/fileuploader/model/OutputMessage.java
@@ -0,0 +1,14 @@
+package com.example.fileuploader.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class OutputMessage {
+
+    private String text;
+
+    private String time;
+
+}
